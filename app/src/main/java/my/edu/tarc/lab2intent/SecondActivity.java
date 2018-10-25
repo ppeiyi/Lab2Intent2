b1c16e271422c44402c37cc9b0534c3dcbcf5139
ppeiyi <lohpy-wa16@student.tarc.edu.my>
2018-10-25 17:04:51 +0800

    Initial Commit

---
+++ app/src/main/java/my/edu/tarc/lab2intent/SecondActivity.java
@@ -0,0 +1,48 @@
+package my.edu.tarc.lab2intent;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    public static final String REPLY_TAG = "my.edu.tarc.lab2intent.REPLY";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        String stringMsg;
+        TextView textViewMsg = findViewById(R.id.textViewReply);
+        //Create an instance of the Intent
+        Intent intent = getIntent(); //Who called me?
+        if(intent.hasExtra(MainActivity.MESSAGE_TAG)){
+            stringMsg = intent.getStringExtra(MainActivity.MESSAGE_TAG);
+            //Eg.: int age = intent.getIntExtra("TAG_AGE", 0);
+            textViewMsg.setText(stringMsg);
+        }
+    }
+
+    public void sendReply(View view){
+        EditText editTextReply;
+        editTextReply = findViewById(R.id.editTextMessage);
+        if(TextUtils.isEmpty((editTextReply.getText()))){
+            editTextReply.setError(getString(R.string.error_reply));
+            return;
+        }
+
+        String stringReply = editTextReply.getText().toString();
+        //Create an instanceof the Intent
+        Intent intent = new Intent();
+        //Pass value to Intent
+        intent.putExtra(REPLY_TAG, stringReply);
+        //Set result to Ok
+        setResult(RESULT_OK, intent);
+        finish();
+
+    }
+}
